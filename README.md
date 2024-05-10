197e1d22dc76e18bc76c2bb90e73d76f9bed8440
jregues <jregues@me.com>
2024-05-10 18:26:55 -0400

    added readme

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-## E-Commerce Back End Application
+# E-Commerce Back End Application
 
-# About
+## About
 This assignment was created for my Module 13 Challenge assignment, in which I was requested to create the back-end code for an E-Commerce application. This application uses sequelize, postgreSQL and Node.js. This was created in accordance to the following User Story and Acceptance Criteria.
 
-# User Story
+## User Story
 ```
 AS A manager at an internet retail company
 I WANT a back end for my e-commerce website that uses the latest technologies
@@ -11,7 +11,7 @@ SO THAT my company can compete with other e-commerce companies
 
 ```
 
-# Acceptance Criteria
+## Acceptance Criteria
 ```
 GIVEN a functional Express.js API
 WHEN I add my database name, PostgreSQL username, and PostgreSQL password to an environment variable file
@@ -27,7 +27,7 @@ THEN I am able to successfully create, update, and delete data in my database
 
 ```
 
-# Screenshots
+## Screenshots
 
 ![screenshot of a get call for all tags](tags-screenshot.png)
 
@@ -35,10 +35,10 @@ THEN I am able to successfully create, update, and delete data in my database
 
 ![a screenshot of a get call for all products](products-screenshot.png)
 
-# How to Use
+## How to Use
 
 Run 'npm install' to install all dependencies, then log into your postgres terminal and run '\i db/schema.sql'. Once you have the database installed, run 'node seeds/index.js' to seed the data and tables into the database and then run 'npm start' or 'nodemon server.js' if you have nodemon installed globally to start the server. Then you will be able to make GET, POST, PUT, and DELETE calls in insomnia. The endpoints are '/api/categories', '/api/products', and '/api/tags'.
 
-# Links
+## Links
 
 https://github.com/jregues/E-Commerce-back-end/tree/main
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,80 +1,42 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', async (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get("/", async (req, res) => {
   try {
     const tags = await Tag.findAll({
-      include: Product
+      include: Product,
     });
-    res.status(200).json(tags)
+    res.status(200).json(tags);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/:id', async (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get("/:id", async (req, res) => {
   const tagId = req.params.id;
   try {
     const tag = await Tag.findOne({
       where: {
-        id: tagId
+        id: tagId,
       },
-      include: Product
+      include: Product,
     });
     if (tag) {
-      res.status(200).json(tag)
+      res.status(200).json(tag);
     } else {
-      res.status(404).json({ message: 'Tag not found' });
+      res.status(404).json({ message: "Tag not found" });
     }
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// router.post('/', (req, res) => {
-//   // create a new tag
-//   Tag.create(req.body)
-//     .then((tag) => {
-//       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-//       if (req.body.tagIds.length) {
-//         const productTagIdArr = req.body.tagIds.map((product_id) => {
-//           return {
-//             tag_id: tag.id,
-//             product_id,
-//           };
-//         });
-//         return ProductTag.bulkCreate(productTagIdArr);
-//       }
-//       // if no product tags, just respond
-//       res.status(200).json(tag);
-//     })
-//     .then((productTagIds) => res.status(200).json(productTagIds))
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(400).json(err);
-//     });
-// });
-
-router.post('/', (req, res) => {   // create a new tag   
+router.post("/", (req, res) => {
+  // create a new tag
   Tag.create(req.body)
     .then((tag) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model       
-      // if (req.body.tagIds.length) {
-      //   const productTagIdArr = req.body.tagIds.map((product_id) => {
-      //     return {
-      //       tag_id: tag.id,
-      //       product_id,
-      //     };
-      //   });
-      //   return ProductTag.bulkCreate(productTagIdArr).then((productTagIds) => res.status(200).json(productTagIds));
-      // }
-      // if no product tags, just respond       
       res.status(200).json(tag);
     })
     .catch((err) => {
@@ -83,7 +45,7 @@ router.post('/', (req, res) => {   // create a new tag
     });
 });
 
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
   try {
     const tag = await Tag.update(req.body, {
@@ -92,30 +54,30 @@ router.put('/:id', async (req, res) => {
       },
     });
     if (tag) {
-      res.status(200).json(tag)
+      res.status(200).json(tag);
     } else {
-      res.status(404).json({ message: 'No tag with this id!' })
-    };
+      res.status(404).json({ message: "No tag with this id!" });
+    }
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
   try {
     const tag = await Tag.destroy({
       where: {
-        id: req.params.id
+        id: req.params.id,
       },
     });
     if (tag) {
-      res.status(200).json(tag)
+      res.status(200).json(tag);
     } else {
-      res.status(404).json({ message: 'No tag found with this id! ' })
+      res.status(404).json({ message: "No tag found with this id! " });
     }
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
